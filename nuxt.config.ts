1597972251968a32edeca5ffc72be0c30bb03f83
NIkita <mykytabaranov@gmail.com>
2025-10-19 13:11:47 +0300

    refactoring

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,14 +4,16 @@ export default defineNuxtConfig({
   app: {
     head: {
       meta: [
-        { name: 'apple-mobile-web-app-title', content: 'Tasks' }
+        { name: 'apple-mobile-web-app-title', content: 'Tasks' },
+        { name: 'theme-color', content: '#ffffff', media: '(prefers-color-scheme: light)' },
+        { name: 'theme-color', content: '#0f172b', media: '(prefers-color-scheme: dark)' }
+
       ],
       link: [
         { rel: 'shortcut icon', type: 'image/x-icon', href: 'favicon.ico' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: 'apple-touch-icon.png' },
         { rel: 'icon', type: 'image/png', sizes: '96x96', href: 'favicon-96x96.png' },
-        { rel: 'icon', type: 'image/svg+xml', href: 'favicon.svg' },
-        { rel: 'manifest', href: '/site.webmanifest' }
+        { rel: 'icon', type: 'image/svg+xml', href: 'favicon.svg' }
       ]
     }
   },
@@ -24,7 +26,8 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@nuxtjs/i18n',
     '@pinia/nuxt',
-    '@vueuse/nuxt'
+    '@vueuse/nuxt',
+    '@vite-pwa/nuxt'
   ],
 
   i18n: {
@@ -75,6 +78,74 @@ export default defineNuxtConfig({
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseKey: process.env.SUPABASE_KEY,
       supabaseBucket: process.env.SUPABASE_BUCKET
+    }
+  },
+
+  pwa: {
+    registerType: 'autoUpdate',
+    includeAssets: [
+      'favicon.ico',
+      'web-app-manifest-192x192.png',
+      'web-app-manifest-512x512.svg'
+    ],
+    manifest: {
+      name: 'Tasks',
+      short_name: 'Tasks',
+      description: 'Collaborate with your team in real time.',
+      display: 'standalone',
+      orientation: 'portrait',
+      start_url: '/',
+      icons: [
+        {
+          "src": "/web-app-manifest-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "maskable"
+        },
+        {
+          "src": "/web-app-manifest-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ]
+    },
+    workbox: {
+      globPatterns: [
+        '**/*.{js,css,html,svg,png,ico,json}'
+      ],
+      runtimeCaching: [
+        {
+        urlPattern: ({ request }) => request.destination === 'document',
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'html-cache'
+          }
+        },
+        {
+        urlPattern: ({ request }) => request.destination === 'script' || request.destination === 'style',
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'asset-cache'
+          }
+        },
+        {
+        urlPattern: ({ request }) => request.destination === 'image',
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'image-cache',
+            expiration: {
+              maxEntries: 100,
+              maxAgeSeconds: 60 * 60 * 24 * 7 // 1 week
+            }
+          }
+        }
+      ]
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallback: '/'
     }
   }
 })
