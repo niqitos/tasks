0def0f2c13ee65146ef7a6a4b1394207c37c581e
NIkita <mykytabaranov@gmail.com>
2025-10-20 11:16:48 +0300

    refactoring

--- server/plugins/subscription-cron.ts
+++ server/plugins/subscription-cron.ts
@@ -2,11 +2,14 @@ import { runDueSubscriptions } from '@@/server/services/charge-scheduler'
 import { CronJob } from 'cron'
 
 export default defineNitroPlugin(() => {
-  const job = new CronJob('0 0 * * *', async () => {
-    console.log('Running subscription scheduler...')
+  const job = new CronJob(
+    '0 0 * * *', // every day at midnight
+    async () => {
+      console.log('Running subscription scheduler...')
 
-    await runDueSubscriptions()
-  })
+      await runDueSubscriptions()
+    }
+  )
 
   job.start()
 
