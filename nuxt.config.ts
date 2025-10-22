caedca10085723f4ff6f635a80c7235fd403b160
NIkita <mykytabaranov@gmail.com>
2025-10-22 11:51:04 +0300

    refactoring

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       meta: [
-        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'mobile-web-app-capable', content: 'yes' },
         { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
         { name: 'apple-mobile-web-app-title', content: 'Tasks' },
         { name: 'theme-color', content: '#ffffff', media: '(prefers-color-scheme: light)' },
